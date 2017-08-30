e28a2343affbe08532b2bdc593740adfed6161a5
Stuart Grant <grantsru@gmail.com>
2017-08-30 09:42:33 -0400

    Hash tables

---
+++ basics/hashtables/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"sort"
+)
+
+func one() {
+	res, err := http.Get("https://raw.githubusercontent.com/dwyl/english-words/master/words.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	bs, _ := ioutil.ReadAll(res.Body)
+	str := string(bs)
+	fmt.Println(str)
+}
+
+func two() {
+	var m map[int]string
+	var keys []int
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	for _, k := range keys {
+		fmt.Println("Key:", k, "Value:", m[k])
+	}
+}
+
+func main() {
+	one()
+	two()
+}
